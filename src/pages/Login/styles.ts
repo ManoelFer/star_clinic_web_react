0dca75b946a386073162531c21f2c79b660cc7dd
ManoelFer <manoelfernandes15@gmail.com>
2022-10-12 12:02:43 -0300

    ⚰️ remove login styles

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    width: 1000px;
-    height: 1000px;
-    background-color: ${(props) => props.theme.colors.backgroundColor};
+
 `
